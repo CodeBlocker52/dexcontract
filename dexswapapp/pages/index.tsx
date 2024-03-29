3bc76d561b64a21e2282930d6f5b7e5dffae8734
Kundan <codeblocker736@gmail.com>
2024-03-29 14:01:09 +0530

    fc

---
+++ dexswapapp/pages/index.tsx
@@ -0,0 +1,14 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}></div>
+    </main>
+  );
+};
+
+export default Home;
